e4f2dc268bbf3a00f495dc41d8ef439e9feafb8c
arturluis <arturluis@dcc.ufmg.br>
2018-05-14 16:29:38 -0300

    Adding data split into folds

--- 3LNN.py
+++ 3LNN.py
@@ -1,5 +1,9 @@
 import numpy as np
-import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from sklearn.model_selection import KFold
+
 import sys
 
 
@@ -7,15 +11,20 @@ def getData(filename='data_tp1'):
     return np.genfromtxt(filename, delimiter=',')
 
 
+def split_data(input_data, folds=5):
+    print('Splitting data in', folds, 'folds')
+    kf = KFold(n_splits=folds, shuffle=True)
+    return(kf.split(input_data))
+
+
 def main(argv):
     if len(argv) > 1:
         filename = argv[1]
-        data = getData(filename)
+        input_data = getData(filename)
     else:
         print("no input file, using default")
-        data = getData()
-
-
+        input_data = getData()
+    split_data(input_data)
 
 if __name__ == '__main__':
     main(sys.argv)
