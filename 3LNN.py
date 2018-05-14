f816b8671e527bc0d758d444cb04836b4ef3188b
arturluis <arturluis@dcc.ufmg.br>
2018-05-14 16:43:31 -0300

    Adding naive data split

--- 3LNN.py
+++ 3LNN.py
@@ -7,8 +7,11 @@ from sklearn.model_selection import KFold
 import sys
 
 
-def getData(filename='data_tp1'):
-    return np.genfromtxt(filename, delimiter=',')
+def get_data(filename='data_tp1'):
+    data = np.genfromtxt(filename, delimiter=',')
+    labels = data[:, 0]
+    imgs = data[:, 1:]
+    return imgs, labels
 
 
 def split_data(input_data, folds=5):
@@ -17,14 +20,28 @@ def split_data(input_data, folds=5):
     return(kf.split(input_data))
 
 
+def naive_split(input_data, labels, train_size=4000):
+    x_train = input_data[:train_size]
+    y_train = labels[:train_size]
+
+    x_test = input_data[train_size:]
+    y_test = labels[train_size:]
+
+    return (x_train, y_train, x_test, y_test)
+
+
 def main(argv):
     if len(argv) > 1:
         filename = argv[1]
-        input_data = getData(filename)
+        imgs, labels = get_data(filename)
     else:
         print("no input file, using default")
-        input_data = getData()
-    split_data(input_data)
+        imgs, labels = get_data()
+
+    x_train, y_train, x_test, y_test = naive_split(imgs, labels)
+    print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+    # split_data(input_data)
+
 
 if __name__ == '__main__':
     main(sys.argv)
