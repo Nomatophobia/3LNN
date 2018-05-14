e5e6fe087cc2f17c3f91843f6bd7d90fe85cdabe
arturluis <arturluis@dcc.ufmg.br>
2018-05-14 15:55:20 -0300

    Read input data

--- 3LNN.py
+++ 3LNN.py
@@ -1,3 +1,21 @@
 import numpy as np
-import tensorflow
 import keras
+import sys
+
+
+def getData(filename='data_tp1'):
+    return np.genfromtxt(filename, delimiter=',')
+
+
+def main(argv):
+    if len(argv) > 1:
+        filename = argv[1]
+        data = getData(filename)
+    else:
+        print("no input file, using default")
+        data = getData()
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
